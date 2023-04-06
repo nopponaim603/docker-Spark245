31e3a768544f7b23f6f20d6e63b5a46572b6ab0d
AIMMY-CAMTPC\noppo <noppon.w@cmu.ac.th>
2023-04-06 16:37:15 +0700

    Add config

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-SPARK_VERSION="3.3.2"
-HADOOP_VERSION="3.3"
+SPARK_VERSION="3.1.2"
+HADOOP_VERSION="2.7"
 JUPYTERLAB_VERSION="3.6.3"
 
 # -- Building the Images
@@ -12,15 +12,15 @@ docker build \
   --build-arg spark_version="${SPARK_VERSION}" \
   --build-arg hadoop_version="${HADOOP_VERSION}" \
   -f spark-base.Dockerfile \
-  -t nopponaim603/spark-base:3.3.2 .
+  -t nopponaim603/spark-base:3.1.2 .
 
 docker build \
   -f spark-master.Dockerfile \
-  -t nopponaim603/spark-master:3.3.2 .
+  -t nopponaim603/spark-master:3.1.2 .
 
 docker build \
   -f spark-worker.Dockerfile \
-  -t nopponaim603/spark-worker:3.3.2 .
+  -t nopponaim603/spark-worker:3.1.2 .
 
 docker build \
   --build-arg spark_version="${SPARK_VERSION}" \
