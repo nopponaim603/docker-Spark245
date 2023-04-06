1a396af7eaf4818af66e2b5470399f6127383f75
AIMMY-CAMTPC\noppo <noppon.w@cmu.ac.th>
2023-04-06 16:00:57 +0700

    setting build to dockerhub @nopponaim603

--- build.sh
+++ build.sh
@@ -1,32 +1,32 @@
-SPARK_VERSION="2.4.5"
-HADOOP_VERSION="2.7"
-JUPYTERLAB_VERSION="2.1.5"
+SPARK_VERSION="3.3.2"
+HADOOP_VERSION="3.3"
+JUPYTERLAB_VERSION="3.6.3"
 
 # -- Building the Images
 
 docker build \
   -f cluster-base.Dockerfile \
-  -t cluster-base .
+  -t nopponaim603/debian-cluster-base .
 
 docker build \
   --build-arg spark_version="${SPARK_VERSION}" \
   --build-arg hadoop_version="${HADOOP_VERSION}" \
   -f spark-base.Dockerfile \
-  -t spark-base .
+  -t nopponaim603/spark-base:3.3.2 .
 
 docker build \
   -f spark-master.Dockerfile \
-  -t spark-master .
+  -t nopponaim603/spark-master:3.3.2 .
 
 docker build \
   -f spark-worker.Dockerfile \
-  -t spark-worker .
+  -t nopponaim603/spark-worker:3.3.2 .
 
 docker build \
   --build-arg spark_version="${SPARK_VERSION}" \
   --build-arg jupyterlab_version="${JUPYTERLAB_VERSION}" \
   -f jupyterlab.Dockerfile \
-  -t jupyterlab .
+  -t nopponaim603/jupyterlab:3.6.3 .
 
 # Local copy of Notebooks and job-submit scripts outside Git change tracking
 mkdir -p ./local/notebooks
